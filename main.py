ea30d5061991e0f952634c90d3d2225133339d90
Oran C <oranbusiness@gmail.com>
2025-04-09 21:07:24 -0700

    80% of he way there

--- main.py
+++ main.py
@@ -1,56 +1,47 @@
 #!/usr/bin/env python3
 # taggercopy Source Code
-
+import dotenv
+import pyperclip
+import anthropic
 import os
-from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
 
-
-client  = OpenAI(
-    base_url="http://localhost:8080/v1",
-    api_key="sk-no-key-required"
+api_key = os.getenv("API_KEY")
+client = anthropic.Anthropic(
+    api_key=api_key
 )
 
-completion = client.chat.completions.create(
-    model="LLaMA_CPP",
-    grammer='''
-<tag>  ::= [a-z]+ | <tags> | <tags_end> | "\n"
-<tags> ::=  [a-z]+ "," <tag>
-<tags_end> ::=  <tags> "\n" ''',
+# add short cut command to trigger pyperclip
+current_clipboard = pyperclip.paste()
+strip_clipboard = current_clipboard.strip()
+
+note_classification_text = strip_clipboard
+# """
+# Idea, printer, automation for making paper checklist every day. Add whisper interface to add new items really quickly in the morning. Add business idea where you make a piece of software and a companion app that allows for this type of bullet journal paper automation. Add Activity Pieces, Common Scripts. add whisper subscription? add android app
+# """
+
+message = client.messages.create(
+    model="claude-3-5-sonnet-20241022",
+    max_tokens=8192,
+    temperature=1,
+    system="act as a note tag extractor. return a list of tags lowercase, ex: \"note,blog,code,idea\". do not be helpful.",
     messages=[
-        { "role": "system", "content": "act as a note tag extractor. return a list of tags lowercase, ex: note,blog,code,idea. do not be helpful."},
-        { "role": "user", "content": "Idea, printer, automation for making paper checklist every day. Add whisper interface to add new items really quickly in the morning. Add business idea where you make a piece of software and a companion app that allows for this type of bullet journal paper automation. Add Activity Pieces, Common Scripts. add whisper subscription? add android app"}
+        {
+            "role": "user",
+            "content": [
+                {
+                "type" : "text",
+                "text" : note_classification_text
+                }
+            ]
+        }
     ]
 )
 
-print(completion.choices[0].message)
-
-
-from llama_index.llms.llamafile import Llamafile
-import pyperclip
-
-llm = Llamafile(temperature=0, seed=0)
-
-resp = llm.complete("act as a obsidian notes tag extractor. example: [[notes]], [[coding]],[[idea]],  do not be helpful, output tags only. heres my note: ")
-
-print(resp)
-
-
-# 
-from llama_index.core.llms import ChatMessage
-
-message = [
-    ChatMessage(
-        role="system",
-        content="act as a obsidian notes tag extractor. example: [[notes]], [[coding]],[[idea]],  do not be helpful, output tags only. heres my note: ",
-    ),
-    ChatMessage(
-        role="user",
-        content="""
-Idea, printer, automation for making paper checklist every day.
-Add whisper interface to add new items really quickly in the morning.
-Add business idea where you make a piece of software and a companion app that allows for this type of bullet journal paper automation.
-Add Activity Pieces, Common Scripts. 
-add whisper subscription?
-add android app"""
-    )
-]
+tag_formated = message.content[0].text.split(",")
+tag_final_form = "".join([ f"[[{tag}]]" for tag in tag_formated ])
+print(message.content[0].text)
+print("output:")
+print(tag_final_form)
+pyperclip.copy(message.content[0].text)
